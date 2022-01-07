9eb1e1973f3fa1130a8432e150dd4da51a919e9e
Ncookie <ncukiert@gmail.com>
2022-01-06 22:08:11 -0300

    Added Axial Distribution Checker Script

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ exports.xsnob = '0x83952E7ab4aca74ca96217D6F8f7591BEaD6D64E';
 exports.gaugeProxy = '0x215D5eDEb6A6a3f84AE9d72962FEaCCdF815BF27';
 exports.oldGaugeProxy = '0xFc371bA1E7874Ad893408D7B581F3c8471F03D2C';
 exports.operations = '0x096a46142C199C940FfEBf34F0fe2F2d674fDB1F';
+exports.council = '0x028933a66DD0cCC239a3d5c2243b2d96672f11F5';
 exports.treasury = '0x294aB3200ef36200db84C4128b7f1b4eec71E38a';
 exports.devFund = '0x88aDa02f6fCE2F1A833cd9B4999D62a7ebb70367';
 exports.feeDistributor = '0xad86ef5fd2ebc25bb9db41a1fe8d0f2a322c7839';
---
+++ scripts/axialDistCheck.js
@@ -0,0 +1,172 @@
+
+// Required Packages:
+const { ethers } = require('ethers');
+const axios = require('axios');
+
+// Required Config Variables:
+const config = require('../config.js');
+
+// Setting Up RPC:
+const avax = new ethers.providers.JsonRpcProvider(config.rpc);
+const avax_backup = new ethers.providers.JsonRpcProvider(config.rpc_backup);
+
+// Setting Time Variables:
+const time = Math.round(Date.now() / 1000);
+const week = 604800;
+
+/* ====================================================================================================================================================== */
+
+// Function to make blockchain queries:
+const query = async (address, abi, method, args) => {
+  let result;
+  let errors = 0;
+  while(!result) {
+    try {
+      let contract = new ethers.Contract(address, abi, avax);
+      result = await contract[method](...args);
+    } catch {
+      try {
+        let contract = new ethers.Contract(address, abi, avax_backup);
+        result = await contract[method](...args);
+      } catch {
+        if(++errors === 5) {
+          console.error(`\n  > Error calling ${method}(${args}) on ${address}`);
+          console.warn(`  > Execution was stopped due to errors. Check script or try again.`);
+          process.exit(1);
+        }
+      }
+    }
+  }
+  return result;
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to get distribution timestamps:
+const getTimestamps = async () => {
+  let timestamps = [];
+  let startTime = parseInt(await query(config.axialFeeDistributor, config.feeDistributorABI, 'start_time', []));
+  let tempTime = startTime;
+  while(tempTime < (time - week)) {
+    timestamps.push(tempTime);
+    tempTime += week;
+  }
+  console.log('Timestamps fetched...');
+  return timestamps;
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to get AXIAL distributions from fee distributor contract:
+const getContractDistributions = async (timestamps) => {
+  let distributions = [];
+  let promises = timestamps.map(timestamp => (async () => {
+    let amount = parseInt(await query(config.axialFeeDistributor, config.feeDistributorABI, 'tokens_per_week', [timestamp])) / (10 ** 18);
+    distributions.push({timestamp, amount});
+  })());
+  await Promise.all(promises);
+  distributions.sort((a, b) => a.timestamp - b.timestamp);
+  console.log('Distributions from contract fetched...');
+  return distributions;
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to get AXIAL distributions from council transactions:
+const getCouncilDistributions = async (timestamps) => {
+  let distributions = [];
+  let txs = await getCouncilTXs();
+  for(let i = 0; i < timestamps.length; i++) {
+    let sum = 0;
+    txs.forEach(tx => {
+      if(i === timestamps.length - 1) {
+        if(tx.timestamp > timestamps[i] && tx.timestamp < time) {
+          sum += tx.amount;
+        }
+      } else {
+        if(tx.timestamp > timestamps[i] && tx.timestamp < timestamps[i + 1]) {
+          sum += tx.amount;
+        }
+      }
+    });
+    distributions.push({timestamp: timestamps[i], amount: sum});
+  }
+  distributions.sort((a, b) => a.timestamp - b.timestamp);
+  console.log('Distributions from council TXs fetched...');
+  return distributions;
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to get council transactions:
+const getCouncilTXs = async () => {
+  let txs = [];
+  let page = 0;
+  let hasNextPage = false;
+  do {
+    try {
+      let query = `https://api.covalenthq.com/v1/43114/address/${config.axialFeeDistributor}/transfers_v2/?contract-address=${config.axial}&page-size=1000&page-number=${page++}&key=${config.ckey}`;
+      let result = await axios.get(query);
+      if(!result.data.error) {
+        hasNextPage = result.data.data.pagination.has_more;
+        let promises = result.data.data.items.map(tx => (async () => {
+          if(tx.successful && tx.to_address.toLowerCase() === config.council.toLowerCase()) {
+            let timestamp = (new Date(tx.block_signed_at)).getTime() / 1000;
+            let amount = tx.transfers[0].delta / (10 ** 18);
+            txs.push({timestamp, amount});
+          }
+        })());
+        await Promise.all(promises);
+        console.log(`Council transactions loaded... (Page ${page})`);
+      } else {
+        hasNextPage = false;
+      }
+    } catch {
+      console.log('API ERROR: Covalent is likely down.');
+      hasNextPage = false;
+    }
+  } while(hasNextPage);
+  return txs;
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to pad date if necessary:
+const pad = (num) => {
+  let str = num.toString();
+  if(str.length < 2) {
+    return '0' + str;
+  } else {
+    return str;
+  }
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to fetch all stats:
+const fetch = async () => {
+
+  // Fetching Timestamps:
+  let timestamps = await getTimestamps();
+
+  // Fetching Data:
+  let contractDistributions = await getContractDistributions(timestamps);
+  let councilDistributions = await getCouncilDistributions(timestamps);
+
+  // Printing Data:
+  console.log('\n  ================================');
+  console.log('  ||     Axial Distributions    ||');
+  console.log('  ================================\n');
+  timestamps.forEach(timestamp => {
+    let rawDate = new Date((timestamp + week) * 1000);
+    let date = pad(rawDate.getUTCDate()) + '/' + pad(rawDate.getUTCMonth() + 1) + '/' + rawDate.getUTCFullYear();
+    console.log(`  - ${date}:`);
+    console.log(`      > Contract:    ${contractDistributions.find(dist => dist.timestamp === timestamp).amount.toLocaleString(undefined, {maximumFractionDigits: 0})} AXIAL`);
+    console.log(`      > Council TXs: ${councilDistributions.find(dist => dist.timestamp === timestamp).amount.toLocaleString(undefined, {maximumFractionDigits: 0})} AXIAL`);
+  });
+}
+
+/* ====================================================================================================================================================== */
+
+// Fetching Data:
+fetch();
