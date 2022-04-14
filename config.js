103a76393d57335a34572dd93623259caef2d3fc
Ncookiez <ncukiert@gmail.com>
2022-04-14 14:11:49 -0300

    Actually Updated Axial Stats

--- config.js
+++ config.js
@@ -126,8 +126,8 @@ exports.axialDistributions = [ // https://api.snowtrace.io/api?module=block&acti
   { block: 12201830, timestamp: 1647475200 }, // Mar. 17, 2022
   { block: 12493787, timestamp: 1648080000 }, // Mar. 24, 2022
   { block: 12791423, timestamp: 1648684800 }, // Mar. 31, 2022
-  { block: 13086674, timestamp: 1649289600 }  // Apr.  7, 2022
-  // { block: , timestamp: 1649894400 }  // Apr. 14, 2022
+  { block: 13086674, timestamp: 1649289600 }, // Apr.  7, 2022
+  { block: 13385655, timestamp: 1649894400 }  // Apr. 14, 2022
   // { block: , timestamp: 1650499200 }  // Apr. 21, 2022
   // { block: , timestamp: 1651104000 }  // Apr. 28, 2022
   // { block: , timestamp: 1651708800 }  // May.  5, 2022
