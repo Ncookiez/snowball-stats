43c9bac2f56dab745d8552b6134f4fdd40692f4e
Ncookiez <ncukiert@gmail.com>
2022-07-07 12:30:32 -0300

    Weekly Update

--- config.js
+++ config.js
@@ -183,8 +183,8 @@ exports.weeklyData = [ // https://api.snowtrace.io/api?module=block&action=getbl
   { block: 15780748, timestamp: 1654732800, snob: 0.036, axial: 0.0019 }, // Jun.  9, 2022
   { block: 16086066, timestamp: 1655337600, snob: 0.024, axial: 0.0017 }, // Jun. 16, 2022
   { block: 16389504, timestamp: 1655942400, snob: 0.025, axial: 0.0015 }, // Jun. 23, 2022
-  { block: 16691758, timestamp: 1656547200, snob: 0.022, axial: 0.0016 }  // Jun. 30, 2022
-  // { block: , timestamp: 1657152000, snob: , axial:  }  // Jul.  7, 2022
+  { block: 16691758, timestamp: 1656547200, snob: 0.022, axial: 0.0016 }, // Jun. 30, 2022
+  { block: 16993029, timestamp: 1657152000, snob: 0.041, axial: 0.0018 }  // Jul.  7, 2022
   // { block: , timestamp: 1657756800, snob: , axial:  }  // Jul. 14, 2022
   // { block: , timestamp: 1658361600, snob: , axial:  }  // Jul. 21, 2022
   // { block: , timestamp: 1658966400, snob: , axial:  }  // Jul. 28, 2022
